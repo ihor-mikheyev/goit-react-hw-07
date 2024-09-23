47c146b0bef6ad0c9eb82142c90bf099a007310e
Ihor Mikheyev <ihor.mikheiev@gmail.com>
2024-09-23 08:11:50 +0200

    finish
    remove persistGate

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
 import { StrictMode } from "react";
 import { Provider } from "react-redux";
 import { createRoot } from "react-dom/client";
-import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "./redux/store.js";
+import { store } from "./redux/store.js";
 import App from "./App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <PersistGate loading={null} persistor={persistor}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </PersistGate>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
